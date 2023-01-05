68520cc4e9eefae9cf162a8cf73583fdd31814cf
mohamedInfath <mohamedinfath99@gmail.com>
2023-01-05 22:55:05 +0530

    slider done

---
+++ Client/src/components/Slider.jsx
@@ -0,0 +1,145 @@
+import styled from 'styled-components';
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons';
+import { useState } from 'react';
+import {sliderItems} from '../data'
+
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+
+    position: relative;
+
+    overflow: hidden;
+`
+
+const Arrow = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: lightgray;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+
+    left: ${props => props.direction === "left" && "10px"};
+    right: ${props => props.direction === "right" && "10px"};
+
+    cursor: pointer;
+    opacity: 0.5;
+
+
+    z-index: 2;
+`
+
+const Warapper = styled.div`
+    height: 100%;
+    display: flex;
+
+    /* slide change code */
+    transform: translateX(${props => props.slideIndex * -100}vw);
+
+    transition: all 1.5s ease;
+`
+
+const Slide = styled.div`
+    width: 100vw; 
+    height: 100vh;
+    display: flex;
+    align-items: center;
+`
+
+
+const ImgContainer = styled.div`
+    flex: 1;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+
+const Image = styled.img`
+    height: 80%;
+`
+
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`
+
+const Title = styled.h1`
+    font-size: 70px;
+`
+
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`
+
+const Slider = () => {
+
+    const [slideIndex, setSlideIndex] = useState(0)
+
+
+    const handleClick = (direction) => {
+        if(direction === "left") {
+            setSlideIndex(slideIndex > 0 ? slideIndex -1 : 2);
+        }
+        else {
+            setSlideIndex(slideIndex < 2 ? slideIndex +1 : 0);
+        }
+    }
+
+
+    return (
+        <Container>
+
+            <Arrow direction="left" onClick={() => handleClick("left")}>
+                <ArrowLeftOutlined />
+            </Arrow>
+
+            <Warapper slideIndex={slideIndex}>
+
+                {sliderItems.map(item => (
+                    <Slide>
+                        <ImgContainer>
+                            <Image src={item.img} />
+                        </ImgContainer>
+
+                        <InfoContainer>
+                            <Title>{item.title}</Title>
+                            <Desc>{item.desc}</Desc>
+                            <Button>SHOW NOW</Button>
+                        </InfoContainer>
+                    </Slide>
+                ))}
+
+            </Warapper>
+
+            <Arrow direction="right" onClick={() => handleClick("right")}>
+                <ArrowRightOutlined />
+            </Arrow>
+
+        </Container>
+    )
+}
+
+export default Slider
---
+++ Client/src/data.js
@@ -0,0 +1,23 @@
+
+
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: 'https://img.freepik.com/premium-photo/closeup-red-blank-t-shirt-with-empty-space-yours-design-white-background-3d-rendering_476612-5218.jpg',
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GRT FLAT 30% OF FOR NEW ARRIVALS",
+    },
+    {
+        id: 1,
+        img: 'https://www.sirri.co.uk/media/catalog/product/cache/5eba6b316dd6e88c1bf7ab6c64c82900/5/_/5_107.jpg',
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GRT FLAT 30% OF FOR NEW ARRIVALS",
+    },
+    {
+        id: 1,
+        img: 'https://media.istockphoto.com/id/1058523492/vector/yellow-raincoat-vector-illustration.jpg?s=612x612&w=0&k=20&c=omVdOXFuiCmTZk52H_9hzb8we-qB4qLHZ6IiSudI_DY=',
+        title: "RAINNY SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GRT FLAT 30% OF FOR NEW ARRIVALS",
+    }
+]
