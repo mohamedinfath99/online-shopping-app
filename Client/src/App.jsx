96a1d2cdf99e17551fba06186f9757fc5e14634c
mohamedInfath <mohamedinfath99@gmail.com>
2023-01-07 01:01:17 +0530

    file modified

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,17 @@
+
+
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ProductList from "./pages/ProductList";
+import ProductPage from "./pages/ProductPage";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
 
 
 function App() {
   return (
     <div>
-      <Home />
+      <Cart/>
     </div>
   );
 }
--- Client/src/data.js
+++ Client/src/data.js
@@ -9,15 +9,70 @@ export const sliderItems = [
         desc: "DON'T COMPROMISE ON STYLE! GRT FLAT 30% OF FOR NEW ARRIVALS",
     },
     {
-        id: 1,
+        id: 2,
         img: 'https://www.sirri.co.uk/media/catalog/product/cache/5eba6b316dd6e88c1bf7ab6c64c82900/5/_/5_107.jpg',
         title: "WINTER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GRT FLAT 30% OF FOR NEW ARRIVALS",
     },
     {
-        id: 1,
+        id: 3,
         img: 'https://media.istockphoto.com/id/1058523492/vector/yellow-raincoat-vector-illustration.jpg?s=612x612&w=0&k=20&c=omVdOXFuiCmTZk52H_9hzb8we-qB4qLHZ6IiSudI_DY=',
         title: "RAINNY SALE",
         desc: "DON'T COMPROMISE ON STYLE! GRT FLAT 30% OF FOR NEW ARRIVALS",
+    }
+]
+
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        title: "LIGHT JACKETS",
+    }
+]
+
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+        id: 2,
+        img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+    {
+        id: 3,
+        img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+        id: 4,
+        img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+        id: 5,
+        img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+        id: 6,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+        id: 7,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+        id: 8,
+        img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
     }
 ]
